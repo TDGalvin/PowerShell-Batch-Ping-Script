474136bc42250e57b3bb0fac61eb974dc61e257f
TDGalvin <86557864+TDGalvin@users.noreply.github.com>
2021-06-27 13:48:03 +0100

    Add files via upload

---
+++ BatchPing.ps1
@@ -0,0 +1,18 @@
+﻿$complist = Get-Content .\IPAddresses.txt
+
+Start-Transcript -Path .\BatchPingLog.txt
+
+foreach($comp in $complist){
+    
+    $pingtest = Test-Connection -ComputerName $comp -Quiet -Count 1 -ErrorAction SilentlyContinue
+
+    if($pingtest){
+
+         Write-Host($comp + " ;online")
+     }
+     else{
+        Write-Host($comp + " ;reachable")
+     }
+     
+}
+Stop-Transcript
